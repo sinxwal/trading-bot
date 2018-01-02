2c01cf6658e8040df6c8bd519d1fb3b684f79b55
Sergey Tskhovrebov <sinxwal@gmail.com>
2018-01-02 11:26:35 +0300

    Project created

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#curPair").select2({
+    placeholder: "Choose pair"
+  });
+});
+
+$(document).ready(function () {
+  $('[data-toggle="popover"]').popover();
+});
